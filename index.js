98b63dcd1558e5c9399d18fd0f1c53d8fc0bdb77
Richard Lim <rlim.richard+flatiron@gmail.com>
2017-09-07 02:32:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function divide(a, b){
 }
 
 function inc(n){
-    return parseInt(n)++
+    return n++
 }
 
 function dec(n){
-    return parseInt(n)--
+    return n--
 }
 
 function makeInt(n){
